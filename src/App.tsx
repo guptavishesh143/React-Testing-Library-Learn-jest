89e573b8a5d98b4eab99ea48e7b078339aae6755
VISHESH GUPTA <vishesh.gupta@aceturtle.com>
2024-06-07 16:31:34 +0530

    getBy and getbyAll class of queries to assesrt if element are present in dom & queryBy and queryByAll if elements are not present in the dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Form } from "./component/application/form";
+import { Skills } from './component/skills/skills';
+const mySkills = ['HTML', 'CSS', 'Data Structure'];
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      {/* <Form /> */}
+      <Skills skills={mySkills}/>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
